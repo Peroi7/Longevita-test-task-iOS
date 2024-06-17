0aebbaf64c74bcab4c18f7f62150f4d2a578bbef
Peroi7 <ivic.petar77@gmail.com>
2024-06-17 15:22:09 +0200

    Initial Commit

---
+++ Longevita-test-task-iOSTests/Longevita_test_task_iOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  Longevita_test_task_iOSTests.swift
+//  Longevita-test-task-iOSTests
+//
+//  Created by Pero Ivic on 17/06/2024.
+//
+
+import XCTest
+@testable import Longevita_test_task_iOS
+
+class Longevita_test_task_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
